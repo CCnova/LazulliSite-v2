b138da205659d763d520e06ac196481f45e13ce0
CCnova <caiocnova1@gmail.com>
2019-12-23 07:56:30 -0300

    Update

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import classes from './About.module.css';
+
+const about = () => {
+    return(
+        <div className={classes.about}>
+            <Container maxWidth="lg">
+
+            </Container>
+        </div>
+    );
+};
+
+export default about;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Home.module.css';
 import Container from '@material-ui/core/Container';
 import MyButton from '../Button/Button';
+import logo from '../../assets/img/lazulilogo7.jpg';
 
 //PROBLEMAS: Mudar tom do azul, muito claro e colocar cartao branco transparente
 
@@ -9,7 +10,11 @@ import MyButton from '../Button/Button';
 const home = () => {
     return(
         <div className={`${classes.home}`}>
-            <Container className={classes.alignTextLeft} maxWidth="sm">
+            <Container className={classes.centerText} maxWidth='lg'>
+                <img className={classes.responsiveImg} src={logo} alt="logo" />
+            </Container>
+    
+            <Container className={`${classes.alignTextLeft} ${classes.textBox}`} maxWidth="sm">
                 <h1 className={`${classes.lightBlue} ${classes.descript}`}>Exemplo Texto</h1>
                 <p className={`${classes.white} ${classes.secondText}`}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempor nisi quam, non consectetur justo auctor non. Curabitur nisl lorem, fringilla quis ligula sit amet, hendrerit sodales mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris a ex mi. Proin in placerat erat, et sollicitudin mauris.
